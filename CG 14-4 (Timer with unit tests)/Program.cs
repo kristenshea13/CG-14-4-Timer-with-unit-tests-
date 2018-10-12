0c50dcec7d01eddecbb864e881bc485c8f0dd53f
Kristen Shea <kstar61@live.com>
2018-10-11 20:52:10 -0500

    Add project files.

---
+++ CG 14-4 (Timer with unit tests)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_14_4__Timer_with_unit_tests_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
